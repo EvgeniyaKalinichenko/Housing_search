2b8c42d6abee566d468988582c3a5776dd14029d
EvgeniyaKalinichenko <gusewa.evgenia2011@yandex.ru>
2025-04-20 22:38:42 -0500

    Final deploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/housing_search/',
+  base: '/Housing_search/',
   plugins: [react()],
 })
 
