9f827db8e0dad37777d28fd9c1b4628d2c3d0125
EvgeniyaKalinichenko <gusewa.evgenia2011@yandex.ru>
2025-04-23 00:06:22 -0500

    fix styles

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,26 +6,29 @@ type HeaderProps = {
   setCurrency: React.Dispatch<React.SetStateAction<"USD" | "CAD">>;
 };
 
-export const Header = ({currency, setCurrency} : HeaderProps) => {
+export const Header = ({ currency, setCurrency }: HeaderProps) => {
   return (
     <>
       <header className={styles.header}>
-  <div className="pageWrapper">
-    <nav className={styles.nav}>
-      <Link to="/" className={styles.link}>Home</Link>
-      <Link to="/search" className={styles.link}>Search</Link>
-      <select
-        value={currency}
-        onChange={(e) => setCurrency(e.target.value as "USD" | "CAD")}
-        className={styles.select}
-      >
-        <option value="USD">$ USD</option>
-        <option value="CAD">$ CAD</option>
-      </select>
-    </nav>
-  </div>
-</header>
-
+        <div className="pageWrapper">
+          <nav className={styles.nav}>
+            <Link to="/" className={styles.link}>
+              Home
+            </Link>
+            <Link to="/search" className={styles.link}>
+              Search
+            </Link>
+            <select
+              value={currency}
+              onChange={(e) => setCurrency(e.target.value as "USD" | "CAD")}
+              className={styles.select}
+            >
+              <option value="USD">$ USD</option>
+              <option value="CAD">$ CAD</option>
+            </select>
+          </nav>
+        </div>
+      </header>
     </>
   );
 };
