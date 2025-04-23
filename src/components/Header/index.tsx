dd15ef1650bbbe100f0e1c8f92b1101bdc249343
EvgeniyaKalinichenko <gusewa.evgenia2011@yandex.ru>
2025-04-22 23:37:54 -0500

    Fix headers style

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -9,31 +9,23 @@ type HeaderProps = {
 export const Header = ({currency, setCurrency} : HeaderProps) => {
   return (
     <>
-      <div className={styles.header}>
-        <Link to="/" className={styles.link}>
-          Home
-        </Link>
-        <Link to="/search" className={styles.link}>
-          Search
-        </Link>
-        <select
-          value={currency}
-          onChange={(e) => setCurrency(e.target.value as "USD" | "CAD")}
-          style={{
-            padding: "6px 12px",
-            borderRadius: "2px",
-            border: "1px solid white",
-            background: "none",
-            color: "#dc9223",
-            textShadow: "0 0 1px black, 0 0 1px black",
-            fontWeight: 500,
-            marginLeft: "16px",
-          }}
-        >
-          <option value="USD">$ USD</option>
-          <option value="CAD">$ CAD</option>
-        </select>
-      </div>
+      <header className={styles.header}>
+  <div className="pageWrapper">
+    <nav className={styles.nav}>
+      <Link to="/" className={styles.link}>Home</Link>
+      <Link to="/search" className={styles.link}>Search</Link>
+      <select
+        value={currency}
+        onChange={(e) => setCurrency(e.target.value as "USD" | "CAD")}
+        className={styles.select}
+      >
+        <option value="USD">$ USD</option>
+        <option value="CAD">$ CAD</option>
+      </select>
+    </nav>
+  </div>
+</header>
+
     </>
   );
 };
